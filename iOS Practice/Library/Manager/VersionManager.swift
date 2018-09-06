53dbe4d958917f6d0c1785e31244e3ee6038e17c
KL <huikldavid@gmail.com>
2018-09-06 23:29:22 +0800

    add version

---
+++ iOS Practice/Library/Manager/VersionManager.swift
@@ -0,0 +1,17 @@
+//
+//  VersionManager.swift
+//  Big2Scorer
+//
+//  Created by KL on 6/9/2018.
+//  Copyright © 2018 KL. All rights reserved.
+//
+
+import Foundation
+
+class VersionManager {
+    
+    static var versionString: String {
+        guard let dictionary = Bundle.main.infoDictionary, let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+}
