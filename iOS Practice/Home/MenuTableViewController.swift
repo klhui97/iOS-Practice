e3ab66b16ee28bb35b9e07f6cb2f432adc99c360
david.hui <david.hui@innopage.com>
2018-10-10 17:36:12 +0800

    ad ar

--- iOS Practice/Home/MenuTableViewController.swift
+++ iOS Practice/Home/MenuTableViewController.swift
@@ -55,6 +55,8 @@ private extension MenuTableViewController {
                 images.append(UIImage(named: "demo\(i)")!)
             }
             return KLImagePageViewController(mode: .autoInfinite, images: images)
+        case .arKits:
+            return ARKitViewController()
         }
     }
 }
@@ -80,9 +82,10 @@ class MenuTableViewController: KLTableViewController {
         case collectionViewPaging = "Collection View Paging"
         case speechRecognition = "Speech Recognition"
         case infinteImagesScroll = "Infinte images scroll"
+        case arKits = "AR Kits"
     }
     
-    var rows: [Row] = [.infinteImagesScroll, .collectionViewPaging, .speechRecognition, .swift4Json, .hkKmbData, .chatUi, .inputObserver, .bluetoothReadWrite, .messageUi, .whatsappApi, .rssParse, .circleProgressView, .magicalGridView, .animationView, .animationButton, .activityIndicator, .popUpView]
+    var rows: [Row] = [.arKits, .infinteImagesScroll, .collectionViewPaging, .speechRecognition, .swift4Json, .hkKmbData, .chatUi, .inputObserver, .bluetoothReadWrite, .messageUi, .whatsappApi, .rssParse, .circleProgressView, .magicalGridView, .animationView, .animationButton, .activityIndicator, .popUpView]
     var delegate: MenuTableViewControllerDelegate?
     
     // MARK: - Life cycle
